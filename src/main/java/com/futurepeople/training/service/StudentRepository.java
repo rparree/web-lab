5fda9a2dd98169ba64220bde1a58a6d9b72e4371
rparree <rparree@edc4it.com>
2013-07-08 11:53:10 +0100

    Implemented the isRegistered method

--- src/main/java/com/futurepeople/training/service/StudentRepository.java
+++ src/main/java/com/futurepeople/training/service/StudentRepository.java
@@ -3,29 +3,37 @@ package com.futurepeople.training.service;
 import com.futurepeople.training.domain.Student;
 
 import javax.annotation.Resource;
-import javax.inject.Singleton;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
  * todo
  */
-@Singleton
+
 public class StudentRepository {
 
   @Resource(lookup = "java:/TrainingDS")
   DataSource dataSource;
 
   public boolean isRegistered(String email) {
-    String sql = "select email from Student where email=?";
+    String sql = "select id from Student where email=?";
 
-    try (Connection connection = dataSource.getConnection()) {
-      System.out.println("Got connection");
-      return false;
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement stm = connection.prepareStatement(sql)
+
+    ) {
+      stm.setString(1, email);
+      try (ResultSet rs = stm.executeQuery()) {
+        return rs.next();
+      }
     } catch (SQLException e) {
       throw new RuntimeException(e);
+
     }
+
 
   }
 
